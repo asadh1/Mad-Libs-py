fc8feb34ecb7067f1142e5aabe362fb278bc3b4a
asadh369 <h.asad369@gmail.com>
2016-11-01 18:48:39 -0400

    Update HashimML-PY-5.py

--- HashimML-PY-5.py
+++ HashimML-PY-5.py
@@ -45,6 +45,7 @@ class Mad_libs():
         continue
     print(data)
 
+# Change the file paths if you move the file or if you're not using a mac
 _taoe = Mad_libs("The Art Of Espionage", "/Users/{}/Downloads/Mad-Libs-py-master/madlibs_str.txt".format(pwd.getpwuid(os.getuid())[0]))
 
 _iyga = Mad_libs("If You Give A •••", "/Users/{}/Downloads/Mad-Libs-py-master/madlibs_str2.txt".format(pwd.getpwuid(os.getuid())[0]))
